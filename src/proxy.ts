fa92edd93d81ab18ef969ba43143d8089bf16c1c
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2025-12-28 00:46:13 +0500

    feat - add AuthContextProvider for session management and implement proxy for authentication redirects

---
+++ src/proxy.ts
@@ -0,0 +1,26 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth"
+
+export async function proxy(request: NextRequest) {
+    const token = await getToken({ req: request })
+    const url = request.nextUrl
+
+    if (
+        token &&
+        (url.pathname === "/sign-in" || url.pathname === "/sign-up" || url.pathname === "/verify")
+    ) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+}
+
+export const config = {
+    matcher: [
+        "/dashboard/:path*",
+        "/profile/:path*",
+        "/sign-in",
+        "/sign-up",
+        "/verify",
+        "/"
+    ]
+}
