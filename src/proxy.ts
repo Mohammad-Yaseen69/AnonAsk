01d01a27b7298ba73f63cf0273f4d4d7a42f153e
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2025-12-28 00:48:13 +0500

    feat: enhance proxy logic for dashboard access redirection based on authentication token

--- src/proxy.ts
+++ src/proxy.ts
@@ -12,6 +12,12 @@ export async function proxy(request: NextRequest) {
     ) {
         return NextResponse.redirect(new URL('/dashboard', request.url))
     }
+
+    if(!token && url.pathname === "/dashboard"){
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+
+    return NextResponse.next()
 }
 
 export const config = {
