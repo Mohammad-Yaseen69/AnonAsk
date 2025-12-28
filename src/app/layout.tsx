325630c242c144c656cd2e83de69cc09596ee7e3
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2025-12-28 23:37:14 +0500

    chore: improve shadcn theme colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+'use client'
+
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 import AuthContextProvider from "@/context/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { useState } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
 
 
 
@@ -8,11 +14,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: 1,
+      }
+    }
+  }))
+
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <AuthContextProvider>
-          {children}
+          <ThemeProvider
+            defaultTheme="dark"
+            enableSystem
+            attribute={"class"}
+            disableTransitionOnChange
+          >
+            <QueryClientProvider client={queryClient}>
+              {children}
+              <Toaster />
+            </QueryClientProvider>
+          </ThemeProvider>
         </AuthContextProvider>
       </body>
     </html>
