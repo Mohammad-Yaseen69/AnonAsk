8b566ae7ee4449b6d9279a51fa6a7ac271866894
Mohammad Yaseen <yaseenyaseenali69@gmail.com>
2026-01-04 15:03:03 +0500

    feat: add API route to fetch public user information by username

---
+++ src/app/api/auth/get-public-info/[username]/route.ts
@@ -0,0 +1,29 @@
+import { ApiResponse } from "@/helpers/apiResponse"
+import dbConnect from "@/lib/dbConnect"
+import User from "@/models/User.model"
+
+export const GET = async (_req: Request, { params }: { params: { username: string } }) => {
+    await dbConnect()
+    try {
+        const param = await params
+        const { username } = param
+
+        const foundUser = await User.findOne({
+            username
+        }).select("fullName isReceivingFeedback username isVerified")
+
+
+        if (!foundUser) {
+            return Response.json(ApiResponse(404, "User not found."), { status: 404 })
+        }
+
+        if (!foundUser.isVerified) {
+            return Response.json(ApiResponse(404, "User is not verified."), { status: 404 })
+        }
+
+        return Response.json(ApiResponse(200, "User fetched successfully", foundUser), { status: 200 })
+    } catch (error) {
+        console.log("Error in get user", error)
+        return Response.json(ApiResponse(500, "Internal Server Error"), { status: 500 })
+    }
+}
--- src/schemas/Replies.validation.ts
+++ src/schemas/Replies.validation.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const CreateReplySchema = z.object({
-    content: z.string().max(500, "Content must be less then 500 character."),
+    content: z.string().max(200, "Content must be less then 200 character."),
     isDirectReply: z.boolean(),
     userId: z.string().min(1, "User id is required"),
     questionId: z.string().optional(),
